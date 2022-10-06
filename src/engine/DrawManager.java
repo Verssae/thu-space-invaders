ddf741d72750c3eff9cdd9473a3a78e9b66212a0
jin <kai5588@naver.com>
2022-10-06 16:37:25 +0900

    [UPDATE] DrawManager에 item spriteMap.put() 메서드 & graphics 추가

--- src/engine/DrawManager.java
+++ src/engine/DrawManager.java
@@ -104,7 +104,7 @@ public final class DrawManager {
 			spriteMap.put(SpriteType.EnemyShipC2, new boolean[12][8]);
 			spriteMap.put(SpriteType.EnemyShipSpecial, new boolean[16][7]);
 			spriteMap.put(SpriteType.Explosion, new boolean[13][7]);
-
+			spriteMap.put(SpriteType.Item, new boolean[8][9]);
 			fileManager.loadSprite(spriteMap);
 			logger.info("Finished loading the sprites.");
 
